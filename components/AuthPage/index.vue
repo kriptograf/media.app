cd2b0227eaa4475f4041881a1cd0a57ba7379e28
foreach <foreach@mail.ru>
2023-01-08 23:07:12 +0300

    Добавлена регистрация и авторизация, центральная колонка

---
+++ components/AuthPage/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="AuthPage">
+
+    </div>
+</template>
+
+<style lang="sass" scoped>
+    @import './index.scss';
+</style>
+
+<script>
+
+
+export default {
+    name: 'AuthPage',
+    data() {
+        return {
+            
+        };
+    },
+    
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+};
+</script>
